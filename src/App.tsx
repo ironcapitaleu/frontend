b2a7f5b716815ab19617edb90a9cb3159422b97d
Demir Catovic <d.catovic9@gmail.com>
2026-03-18 13:36:42 +0100

    refactor: show `PageNotFound` instead of old `Screener`  page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import StockScreener from "./pages/StockScreener";
 import CompanySearch from "./pages/CompanySearch";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
@@ -18,7 +17,6 @@ function App() {
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/" element={<Layout />}>
-						<Route path="/screener" element={<StockScreener />} />
 						<Route path="/search" element={<CompanySearch />} />
 						<Route path="/contact" element={<ContactPage />} />
 						<Route path="/login" element={<LoginPage />} />
